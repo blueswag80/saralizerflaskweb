ea94d1d652f00280383b7270439e9b09d0461caa
Michael.Murphy <michael.murphy@vertias.com>
2017-08-18 21:21:23 -0400

    Setup Basic Functionality and tested generating a basic graph. Upload functionality added as wel.

--- saralizerflaskweb/saralizerflaskweb/__init__.py
+++ saralizerflaskweb/saralizerflaskweb/__init__.py
@@ -3,6 +3,10 @@ The flask application package.
 """
 
 from flask import Flask
+
 app = Flask(__name__)
+UPLOAD_FOLDER = "C:\\Users\\michael.murphy\\Documents\\Visual Studio 2017\\Projects\\saralizerflaskweb\\upload"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["SECRET_KEY"] = '\xe8K\x0cx\x06\x8c8:\xb1n1\xd2{\x19{\xafV\xe9z\xc2\x04y<\x19'
 
 import saralizerflaskweb.views
--- saralizerflaskweb/saralizerflaskweb/views.py
+++ saralizerflaskweb/saralizerflaskweb/views.py
@@ -2,9 +2,21 @@
 Routes and views for the flask application.
 """
 
+import os
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from saralizerflaskweb import app
+from werkzeug.utils import secure_filename
+from saralizerflaskweb.filelogic import SarFile
+from saralizerflaskweb.sarparser import sarparser
+import json
+
+ALLOWED_EXTENSIONS = set(['gz', 'zip', 'sar'])
+
+#Utility function for file uploads.
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +47,27 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    """Renders the upload and handles upload logic"""
+    if request.method == "POST":
+
+        if 'file' not in request.files:
+            flash('No file part received')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            sarguy = sarparser(SarFile(filename, app.config['UPLOAD_FOLDER']))
+            sarinfo = sarguy.analyzeSarLog()
+            cpudatatemp = []
+            for item in sarinfo:
+                cpudatatemp.append([item[0] + item[1], float(item[3])])
+
+            return render_template('graph.html', cpudata=json.dumps(cpudatatemp))
