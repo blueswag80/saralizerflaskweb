e247f8fb365f505e481ee5b5efd592846df51ad5
Michael Murphy <blueswag80@gmail.com>
2017-09-17 15:19:24 -0400

    Can now display data from multiple analyzed sar logs. Added the ability to sort those logs based on day.

--- saralizerflaskweb/saralizerflaskweb/views.py
+++ saralizerflaskweb/saralizerflaskweb/views.py
@@ -5,11 +5,12 @@ Routes and views for the flask application.
 import os
 import json
 from datetime import datetime
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, flash, session
 from saralizerflaskweb import app
 from werkzeug.utils import secure_filename
-from saralizerflaskweb.filelogic import SarFile, SarZipFile
+from saralizerflaskweb.filelogic import SarFile, SarZipFile, remove_sar_logs, setup_upload_directory
 from saralizerflaskweb.sarparser import SarParser
+from saralizerflaskweb.util import get_uuid
 from werkzeug.exceptions import RequestEntityTooLarge
 
 
@@ -25,6 +26,10 @@ def allowed_file(filename):
 @app.route('/')
 @app.route('/home')
 def home():
+
+    if not session.get('uid'):
+        session['uid'] = get_uuid()
+
     """Renders the home page."""
     return render_template(
         'index.html',
@@ -70,29 +75,43 @@ def upload():
             flash('No file selected')
             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
+        if file:
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            uploadpath = os.path.join(app.config['UPLOAD_FOLDER'], session['uid'])
+            fullFilePath = os.path.join(uploadpath, filename)
+            sardata = []
+            setup_upload_directory(uploadpath)
+            file.save(fullFilePath)
             
-            if not SarZipFile.check_is_zipfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+            if not SarZipFile.check_is_zipfile(fullFilePath):
                 flash('You did not upload a zip file or it is corrupt.')
                 return render_template('index.html')
             
-            zipfile = SarZipFile(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            zipfile = SarZipFile(fullFilePath)
 
-            if zipfile.check_Uncompressed_Size() > app.config['MAX_ZIP_SIZE']:
+            if zipfile.check_uncompressed_size() > app.config['MAX_ZIP_SIZE']:
                 flash('The zip file you uploaded would be too large to uncompress.')
                 return render_template('index.html')
 
-            zipfile.unzip(os.path.join(app.config['UPLOAD_FOLDER'], 'outfolder'))
-
-            sarguy = SarParser(SarFile('sar01', app.config['UPLOAD_FOLDER'] + '/outfolder'))
-            sarinfo = sarguy.analyze_sar_log()
+            zipfile.unzip(os.path.join(uploadpath, 'outfolder'))
+            for rootpath, _, files in os.walk(os.path.join(uploadpath, 'outfolder')):
+                for foundfile in files:
+                    sarguy = SarParser(SarFile(foundfile, rootpath))
+                    temp = sarguy.analyze_sar_log()
+                    temp['day'] = int(foundfile.strip('sar'))
+                    sardata.append(temp)
+            # sarguy = SarParser(SarFile('sar04', os.path.join(uploadpath, 'outfolder')))
+            # sarinfo = sarguy.analyze_sar_log()
+            # cpudata = [[cpudata['day'], cpudata['cpuinfo'][0] + cpudata['cpuinfo'][1], float(cpudata['cpuinfo'][3])] for cpudata in sardata]
             cpudatatemp = []
-            print(sarinfo['memoryinfo'])
-            for item in sarinfo['cpuinfo']:
-                cpudatatemp.append([item[0] + item[1], float(item[3])])
+            # cpuddaytemps = []
+            for sarlog in sorted(sardata, key=lambda log: log['day']):
+                for cpu in sarlog['cpuinfo']:
+                    cpudatatemp.append([cpu[0] + cpu[1], float(cpu[3]), 'Day:' + str(sarlog['day']) + ' \n' +
+                                        'Time:' + cpu[0] + cpu[1] + '\n' + 'CPU Percentage:' + cpu[3]])
 
+            # cpudatatemp.append([data['day'], data['cpuinfo'][0] + data['cpuinfo'][1], float(data['cpuinfo'][3])])
+            remove_sar_logs(uploadpath)
             return render_template('graph.html', cpudata=json.dumps(cpudatatemp))
 
 
